c3513e1cdcd14c8df2a3c2d65082bd56471d0917
Akshita Semwal <akshitasemwal@gmail.com>
2023-07-24 22:43:26 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -40,11 +40,17 @@ class Solution{
         boolean[][] dp = new boolean[n+1][sum+1];
         for(int i = 0; i<n+1; i++)
         {
-            dp[i][0] = true;
-        }
-        for(int i = 1; i <sum+1; i++)
-        {
-            dp[0][i] = false;
+            for(int j = 0; j< sum+1; j++)
+            {
+                if( i==0 )
+                {
+                    dp[i][j] = false;
+                }
+                if( j == 0 )
+                {
+                    dp[i][j] = true;
+                }
+            }
         }
         for(int i = 1; i< n+1; i++)
         {
