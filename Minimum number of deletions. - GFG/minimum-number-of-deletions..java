8e62d76d163bd2758affb75585cd77ebd8741fa8
Akshita Semwal <akshitasemwal@gmail.com>
2023-08-16 12:24:18 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions. - GFG/minimum-number-of-deletions..java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0){
+            int n = Integer.parseInt(read.readLine());
+            String str = read.readLine().strip();
+            Solution ob = new Solution();
+            long ans = ob.minDeletions(str, n); 
+            System.out.println(ans);
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    int minDeletions(String s, int n)
+    {
+        StringBuilder sb = new StringBuilder(s);
+        String t = sb.reverse().toString();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 0; i<n+1; i++)
+        {
+            for(int j = 0; j<m+1; j++)
+            {
+                if( i==0 || j==0 )
+                {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i<n+1; i++)
+        {
+            for(int j = 1; j<m+1; j++)
+            {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return n - dp[n][m];
+    }
+} 
