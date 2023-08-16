70069ca136783f97d769afb3f04981e256538fe1
Akshita Semwal <akshitasemwal@gmail.com>
2023-08-16 12:16:31 +0530

    Added solution - LeetHub

---
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Solution obj = new Solution();
+                    System.out.println(obj.longestPalinSubseq(s));
+                }
+                
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int longestPalinSubseq(String s)
+    {
+        StringBuilder sb = new StringBuilder(s);
+        String t = sb.reverse().toString();
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i = 0; i<n+1; i++)
+        {
+            for(int j = 0; j<m+1; j++)
+            {
+                if( i==0 || j==0 )
+                {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i<n+1; i++)
+        {
+            for(int j = 1; j<m+1; j++)
+            {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
