683eefd374a3d9a1687bdb1e86845c60b036deb5
Akshita Semwal <akshitasemwal@gmail.com>
2023-11-01 20:27:24 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -51,33 +51,24 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     static int knapSack(int w, int wt[], int val[], int n) 
     { 
-         int[][] dp = new int[n+1][w+1];
-         for(int i = 0; i < n + 1; i++)
-         {
-             for(int j = 0; j < w + 1; j++)
-             {
-                 if( i == 0 || j == 0 )
-                 {
-                     dp[ i ][ j ] = 0;
-                 }
-             }
-         }
-         
-         for(int i = 1; i < n + 1; i++)
-         {
-             for(int j = 1; j <  w + 1; j++)
-             {
-                 if( wt[i - 1] <= j )
-                 {
-                     dp[ i ][ j ] = Math.max( val[ i - 1 ] + dp[ i - 1 ][ j - wt[ i - 1 ]], dp[ i - 1 ][ j ]);
-                 }
-                 else
-                 {
-                     dp[ i ][ j ] = dp [ i - 1 ][ j ];
-                 }
-             }
-         }
-         return dp[ n ][ w ];
+        int[][] dp = new int[n + 1][w + 1];
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=w; j++)
+            {
+                int currWeight = j - wt[i - 1];
+                int take = Integer.MIN_VALUE;
+                int notTake = dp[i-1][j];
+                if(currWeight >= 0)
+                {
+                    take = val[i - 1] + dp[i - 1][currWeight];
+                    dp[i][j] = Math.max(take, notTake);
+                }
+                dp[i][j] = Math.max(take, notTake);
+            }
+        }
+        //System.out.println(Arrays.deepToString(dp));
+        return dp[n][w];
     } 
 }
 
